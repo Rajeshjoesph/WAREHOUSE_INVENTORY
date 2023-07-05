5e4afe3c97cc5af4e3eabfb1755bfdf178617751
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-07-05 09:08:58 -0700

    Added: Display Inventory data

--- index.js
+++ index.js
@@ -3,30 +3,55 @@ const { log } = require('console');
 const fs=require('fs');
 writeData=require("./inventory");
 
-    let Pcode=prompt("Enter PCode:");
-    let Desc=prompt("Enter Descripation:");
-    let Category=prompt("Enter Category:");
-    let Mrp=prompt("Enter MRP:");
-    let Upc=prompt("Enter Upc:");
+console.log("1 - Inventory entiers:");
+console.log("2 - Display inventory:");
 
-    var inventory={
-        "Pcode":Pcode,
-        "Desc":Desc,
-        "Category":Category,
-        "Mrp":Mrp,
-        "Upc":Upc
-    };
+let process=parseFloat( prompt("Enter the Operation as Press " ));
+switch (process) {
+    case 1:
+        function inventory() {
+            let Pcode=prompt("Enter PCode:");
+            let Desc=prompt("Enter Descripation:");
+            let Category=prompt("Enter Category:");
+            let Mrp=prompt("Enter MRP:");
+            let Upc=prompt("Enter Upc:");
+            let Location=prompt("Enter Stock Location:");
 
-    writeData.push(inventory);
-    obj=JSON.stringify(writeData);
-    fs.writeFileSync("inventory.json",obj);
-
-    let output=fs.readFileSync('inventory.json');
+        
+            var inventory={
+                "Pcode":Pcode,
+                "Desc":Desc,
+                "Category":Category,
+                "Mrp":Mrp,
+                "Upc":Upc,
+                "Location":Location
+            };
+        
+            writeData.push(inventory);
+            obj=JSON.stringify(writeData);
+            fs.writeFileSync("inventory.json",obj);
+        
+            input=parseInt( prompt("IF you want enter data, Press 1 ,Then  Exist Press 0:"));
     
-    let data=JSON.parse(output);
+        }
+        inventory()
+            
+            if (input == 0) {
+                console.log("Inventory data is Added");
+                return false;
+            }else{
+                inventory()
+            }
+        break;
+    case 2:
 
-    console.table(data);
-    
+        let output=fs.readFileSync('inventory.json');
+        let data=JSON.parse(output);
+        console.table(data);
+        break;
 
+    default:
+        console.log("enter the values");
+    break;
 
-    
+}
